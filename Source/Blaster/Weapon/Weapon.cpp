8499336cc9c9db89791725fe0a5236089d7f74c5
YoussefSS <joesamir98@gmail.com>
2022-04-17 16:42:49 +0200

    41. Weapon Class

---
+++ Source/Blaster/Weapon/Weapon.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon.h"
+#include "Components/SphereComponent.h"
+
+AWeapon::AWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false; // FALSE
+	bReplicates = true; // For replication
+
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
+	WeaponMesh->SetupAttachment(RootComponent);
+	SetRootComponent(WeaponMesh);
+
+	WeaponMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);// Later when we drop this weapon, we want it to bounce off of the ground/walls
+	WeaponMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECR_Ignore); // So that when we drop the weapon, the character doesn't get blocked by it
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision); // We set it to no collision at first because in the beginning state it's gonna be on the floor ready to be equipped. When we want to enable collision we can set it to blocking/physics then
+
+	AreaSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AreaSphere"));
+	AreaSphere->SetupAttachment(RootComponent);
+	AreaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore); // We set it to ignore, because in a multiplayer game, we only want this logic to happen on the server. So we can enable it on the server in beginplay
+	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision); // Same as above
+}
+
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (HasAuthority()) // Or use GetLocalRole() == ENetRole::ROLE_Authority
+	{
+		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	}
+}
+
+void AWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
