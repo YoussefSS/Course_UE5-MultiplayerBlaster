5ab3ec10f5e3e03bdf647c9ee5e2562de41ed60f
YoussefSS <joesamir98@gmail.com>
2022-05-01 02:09:20 +0200

    78. Shell Physics

--- Source/Blaster/Weapon/Casing.cpp
+++ Source/Blaster/Weapon/Casing.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Casing.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
 
 ACasing::ACasing()
 {
@@ -9,11 +11,34 @@ ACasing::ACasing()
 
 	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
 	SetRootComponent(CasingMesh);
+	CasingMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECR_Ignore);
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetEnableGravity(true);
+	CasingMesh->SetNotifyRigidBodyCollision(true); // We need this so that OnComponentHit events happen when we have SetSimulatePhysics to true
+	ShellEjectionImpulse = 10.f;
 }
 
 void ACasing::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (CasingMesh)
+	{
+		CasingMesh->OnComponentHit.AddDynamic(this, &ACasing::OnHit);
+		CasingMesh->AddImpulse(GetActorForwardVector() * ShellEjectionImpulse);
+	}
+	
+	SetLifeSpan(2.f); // Destroy after 3 seconds
+}
+
+void ACasing::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (ShellSound)
+	{
+		// We can also do something more fancy like waiting a few seconds before destroying so that the shells linger a bit on the ground
+		UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+	}
+	
+	CasingMesh->SetNotifyRigidBodyCollision(false); // Disable more collision so we don't play the sound again
 }
 
