2374b8aa57b3a8fc133b43e171ccf14a4074489c
YoussefSS <joesamir98@gmail.com>
2022-05-05 14:41:53 +0200

    81. Drawing the Crosshairs

--- Source/Blaster/HUD/BlasterHUD.cpp
+++ Source/Blaster/HUD/BlasterHUD.cpp
@@ -6,4 +6,58 @@
 void ABlasterHUD::DrawHUD()
 {
 	Super::DrawHUD();
+
+	FVector2D ViewportSize;
+	if (GEngine)
+	{
+		GEngine->GameViewport->GetViewportSize(ViewportSize);
+		const FVector2D ViewportCenter(ViewportSize.X / 2.f, ViewportSize.Y / 2.f);
+
+		if (HUDPackage.CrosshairsCenter)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsCenter, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsLeft)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsLeft, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsRight)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsRight, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsTop)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsTop, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsBottom)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsBottom, ViewportCenter);
+		}
+	}
+}
+
+void ABlasterHUD::DrawCrosshair(UTexture2D* Texture, FVector2D ViewportCenter)
+{
+	const float TextureWidth = Texture->GetSizeX();
+	const float TextureHeight = Texture->GetSizeY();
+
+	// If we draw exactly at the center, we are going to draw it with its upper left corner at the center. We need to move it to the left and up by half of the textures size
+	const FVector2D TextureDrawPoint(
+		ViewportCenter.X - (TextureWidth / 2.f),
+		ViewportCenter.Y - (TextureHeight / 2.f)
+	);
+
+	// DrawTexture is in the base HUD class
+	DrawTexture(
+		Texture,
+		TextureDrawPoint.X, // Where the texture will be drawn 
+		TextureDrawPoint.Y, // Where the texture will be drawn 
+		TextureWidth, // Width of the texture
+		TextureHeight, // Height of the texture
+		0.f, // We already did the math up, so no need to adjust these
+		0.f, // We already did the math up, so no need to adjust these
+		1.f, // Don't mess with this, keep it 1.f
+		1.f, // Don't mess with this, keep it 1.f
+		FLinearColor::White // Draw at original color
+	);
 }
