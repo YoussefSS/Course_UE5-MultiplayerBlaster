1993e8a9277f0c33f61e757c5afa9cee89694b0f
YoussefSS <joesamir98@gmail.com>
2022-05-07 15:38:33 +0200

    95. Health

--- Source/Blaster/HUD/BlasterHUD.h
+++ Source/Blaster/HUD/BlasterHUD.h
@@ -34,7 +34,17 @@ public:
 
 	virtual void DrawHUD() override;
 
+	UPROPERTY(EditAnywhere, Category = "Player Stats")
+	TSubclassOf<class UUserWidget> CharacterOverlayClass;
+
+	class UCharacterOverlay* CharacterOverlay;
+
+protected:
+	virtual void BeginPlay() override;
+	void AddCharacterOverlay();
+
 private:
+
 	FHUDPackage HUDPackage;
 
 	void DrawCrosshair(UTexture2D* Texture, FVector2D ViewportCenter, FVector2D Spread, FLinearColor CrosshairColor);
