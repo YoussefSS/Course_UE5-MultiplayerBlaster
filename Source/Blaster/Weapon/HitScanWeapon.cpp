a496af36879153f402c35e85693423d0155d0cdf
YoussefSS <joesamir98@gmail.com>
2022-06-10 17:56:19 +0200

    136. Hit Scan Weapons

---
+++ Source/Blaster/Weapon/HitScanWeapon.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HitScanWeapon.h"
+#include "Engine/SkeletalMeshSocket.h"
+#include "Blaster/Character/BlasterCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+// Note that fire is called on all clients through UCombatComponent::MulticastFire_Implementation
+void AHitScanWeapon::Fire(const FVector& HitTarget)
+{
+	Super::Fire(HitTarget);
+
+	APawn* OwnerPawn = Cast<APawn>(GetOwner());
+	if (OwnerPawn == nullptr) return;
+	AController* InstigatorController = OwnerPawn->GetController();
+
+	const USkeletalMeshSocket* MuzzleFlashSocket = GetWeaponMesh()->GetSocketByName("MuzzleFlash");
+	if (MuzzleFlashSocket && InstigatorController)
+	{
+		FTransform SocketTransform = MuzzleFlashSocket->GetSocketTransform(GetWeaponMesh());
+		FVector Start = SocketTransform.GetLocation();
+		FVector End = Start + (HitTarget - Start) * 1.25f; // If it was exactly at the hit target, we might not get a blocking hit
+
+		FHitResult FireHit;
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			World->LineTraceSingleByChannel(
+				FireHit,
+				Start,
+				End,
+				ECollisionChannel::ECC_Visibility);
+
+			if (FireHit.bBlockingHit)
+			{
+				ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(FireHit.GetActor());
+				if (BlasterCharacter)
+				{
+					if (HasAuthority()) // We only apply damage if we have authority, but we still do the line trace, spawn particles, etc on all clients
+					{
+						UGameplayStatics::ApplyDamage(
+							BlasterCharacter, // damaged actor
+							Damage,
+							InstigatorController, // instigator
+							this, // damage causer
+							UDamageType::StaticClass()
+						);
+					}
+				}
+
+				if (ImpactParticles)
+				{
+					UGameplayStatics::SpawnEmitterAtLocation(
+						World,
+						ImpactParticles,
+						FireHit.ImpactPoint,
+						FireHit.ImpactNormal.Rotation()
+					);
+				}
+
+			}
+		}
+	}
+}
